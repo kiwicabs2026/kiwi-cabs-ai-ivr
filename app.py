cb7298c6763f02f164dc67928147435c3ba57c48
kiwicabs2026 <kiwi.cabs@xtra.co.nz>
2025-07-02 10:40:35 +1200

    update cancel 10:40 Am

--- app.py
+++ app.py
@@ -392,8 +392,7 @@ def cancel_taxicaller_booking(order_id):
         import json
         token_data = json.loads(jwt_token)
         token = token_data['token']
-        
-        cancel_url = f"https://api-rc.taxicaller.net/api/v1/booker/order/{order_id}/cancel"
+        cancel_url = f"https://api-rc.taxicaller.net/api/v1/order/{order_id}/cancel"
         
         headers = {
             "Authorization": f"Bearer {token}",
